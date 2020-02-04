0d1733b003403db4bbf57181fe4bfcf144d958b3
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2020-02-04 15:19:30 +0100

    Add encryption function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use num::traits::{One, ToPrimitive};
+use num::traits::{One, ToPrimitive, Zero};
 use num::BigInt;
 
 #[derive(PartialEq)]
@@ -40,7 +40,27 @@ impl PrivateKey {
   }
 }
 
-pub struct PlainText {}
+pub struct PlainText(BigInt);
+impl PlainText {
+  fn new(m: &BigInt, n: &BigInt) -> Option<Self> {
+    if m >= &BigInt::zero() && m < n {
+      Some(PlainText(m.clone()))
+    } else {
+      None
+    }
+  }
+}
+
+pub struct CipherText(BigInt);
+impl CipherText {
+  fn new(c: BigInt, n_square: BigInt) -> Option<Self> {
+    if c >= Zero::zero() && c < n_square {
+      Some(CipherText(c))
+    } else {
+      None
+    }
+  }
+}
 
 pub fn generate_keypair() -> Option<(PublicKey, PrivateKey)> {
   use num::bigint::RandBigInt;
@@ -56,7 +76,7 @@ pub fn generate_keypair() -> Option<(PublicKey, PrivateKey)> {
   );
   let lambda = lcm(&p - BigInt::one(), &q - BigInt::one());
   let n_square = (&n) * (&n);
-  assert!(n_square > num::traits::Zero::zero());
+  assert!(&n_square > &num::traits::Zero::zero());
   let mut rng = rand::thread_rng();
   let bits = num::bigint::RandomBits::new(n_square.bits());
   let g = rng.sample(bits);
@@ -80,9 +100,34 @@ pub fn generate_keypair() -> Option<(PublicKey, PrivateKey)> {
   ))
 }
 
+fn encrypt(plaintext: &PlainText, key: &PublicKey) -> CipherText {
+  use quick_maths::{gcd, power_mod};
+  let PublicKey {
+    ref n,
+    ref g,
+    ref n_square,
+    ..
+  } = key;
+  // FIXME: This is not random at all! Need to choose r s.t. 0 < r < n, with gcd(r,n) = 1
+  let mut r = n - BigInt::one();
+  while gcd(&r, &n) != One::one() {
+    r -= BigInt::one();
+  }
+  let r = r;
+  assert_eq!(gcd(&r, &n), BigInt::one());
+
+  let PlainText(ref m) = plaintext;
+  let c = (power_mod(g, m, n_square) * power_mod(&r, n, n_square)) % n_square;
+  CipherText(c)
+}
+
 #[test]
-fn same() {
-  let _x = generate_keypair().expect("Generation success");
+fn can_encrypt() {
+  use num::traits::FromPrimitive;
+  let (public_key, private_key) = generate_keypair().expect("Couldn't generate keypair");
+  let plaintext = PlainText::new(&BigInt::from_u64(123).unwrap(), &public_key.n)
+    .expect("Couldn't encode plaintext");
+  let ciphertext = encrypt(&plaintext, &public_key);
 }
 
 mod quick_maths {
