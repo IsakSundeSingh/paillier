aabc9284b24e8dc53913fb216e45ee428f8c6eff
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2020-02-05 18:55:33 +0100

    Clean up some code

--- src/lib.rs
+++ src/lib.rs
@@ -89,27 +89,35 @@ impl Mul<PlainText> for CipherText {
 pub fn generate_keypair() -> Option<(PublicKey, PrivateKey)> {
   use quick_maths::*;
   use rand::Rng;
+
   let bits = 256;
   let p = gen_prime(bits)?;
   let q = gen_prime(bits)?;
   let n = (&p) * (&q);
+
   assert_eq!(
     gcd(&n, &(&(&p - BigInt::one()) * &(&q - BigInt::one()))),
     BigInt::one()
   );
+
   let lambda = lcm(&(&p - BigInt::one()), &(&q - BigInt::one()));
   let n_square = (&n) * (&n);
+
   assert!(&n_square > &num::traits::Zero::zero());
+
   let mut rng = rand::thread_rng();
   let bits = num::bigint::RandomBits::new(n_square.bits());
   let g = rng.sample(bits);
+
   // If mu doesn't exist the generation failed
   let pmod = power_mod(&g, &lambda, &n_square);
   let l_value = l_function(&pmod, &n);
   let mu = mod_inv(&l_value, &n)?;
   //.expect(&format!("n ({}) does not divide g ({})", n, g));
+
   let p_square = (&p) * (&p);
   let q_square = (&q) * (&q);
+
   Some((
     PublicKey { g, n, n_square },
     PrivateKey {
@@ -125,17 +133,20 @@ pub fn generate_keypair() -> Option<(PublicKey, PrivateKey)> {
 
 pub fn encrypt(plaintext: &PlainText, key: &PublicKey) -> Option<CipherText> {
   use quick_maths::{gcd, power_mod, Modulo};
+
   let PublicKey {
     ref n,
     ref g,
     ref n_square,
     ..
   } = key;
+
   // FIXME: This is not random at all! Need to choose r s.t. 0 < r < n, with gcd(r,n) = 1
   let mut r = n - BigInt::one();
   while gcd(&r, &n) != One::one() {
     r -= BigInt::one();
   }
+
   let r = r;
   assert_eq!(gcd(&r, &n), BigInt::one());
 
@@ -146,10 +157,12 @@ pub fn encrypt(plaintext: &PlainText, key: &PublicKey) -> Option<CipherText> {
 
 pub fn decrypt(ciphertext: &CipherText, key: &PrivateKey) -> Option<PlainText> {
   use quick_maths::{l_function, power_mod, Modulo};
+
   let CipherText {
     data,
     n_square: cipher_n_square,
   } = ciphertext;
+
   let PrivateKey {
     ref lambda,
     ref mu,
@@ -171,11 +184,14 @@ pub fn decrypt(ciphertext: &CipherText, key: &PrivateKey) -> Option<PlainText> {
 #[test]
 fn can_encrypt_and_decrypt() {
   use num::traits::FromPrimitive;
+
   let (public_key, private_key) = generate_keypair().expect("Couldn't generate keypair");
   let plaintext = PlainText::new(&BigInt::from_u64(123).unwrap(), &public_key.n)
     .expect("Couldn't encode plaintext");
+
   let ciphertext = encrypt(&plaintext, &public_key).expect("Couldn't encrypt plaintext");
   let decrypted = decrypt(&ciphertext, &private_key).expect("Couldn't decrypt ciphertext");
+
   assert_eq!(plaintext, decrypted);
 }
 
@@ -184,6 +200,7 @@ mod tests {
   use super::*;
   use num::traits::{FromPrimitive, ToPrimitive};
   use proptest::prelude::*;
+
   proptest! {
     #![proptest_config(ProptestConfig { cases: 25, ..ProptestConfig::default() })]
 
@@ -194,7 +211,9 @@ mod tests {
       let (public_key, private_key) = generate_keypair().expect("Key generation failed");
       let c1 = encrypt(&p1, &public_key).expect("c1 encryption failed");
       let c2 = encrypt(&p2, &public_key).expect("c2 encryption failed");
+
       let c = c1 + c2;
+
       let PlainText(decrypted) = decrypt(&c, &private_key).expect("Couldn't decrypt result!");
       assert_eq!(x + y, decrypted.to_u64().expect("Couldn't convert decrypted result to u64"));
     }
@@ -205,7 +224,9 @@ mod tests {
       let p2 = PlainText(BigInt::from_u64(y).unwrap());
       let (public_key, private_key) = generate_keypair().expect("Key generation failed");
       let c1 = encrypt(&p1, &public_key).expect("c1 encryption failed");
+
       let c = c1 * p2;
+
       let PlainText(decrypted) = decrypt(&c, &private_key).expect("Couldn't decrypt result!");
       assert_eq!(x * y, decrypted.to_u64().expect("Couldn't convert decrypted result to u64"));
     }
@@ -213,14 +234,17 @@ mod tests {
 }
 
 mod quick_maths {
-  use num::traits::{FromPrimitive, One, Zero};
+  use num::traits::{One, Zero};
   use num::BigInt;
+
   pub(crate) fn lcm(a: &BigInt, b: &BigInt) -> BigInt {
     num::integer::lcm(a.clone(), b.clone())
   }
+
   pub(crate) fn gcd(a: &BigInt, b: &BigInt) -> BigInt {
     num::integer::gcd(a.clone(), b.clone())
   }
+
   pub(crate) fn mod_inv(a: &BigInt, b: &BigInt) -> Option<BigInt> {
     let x = mod_inv2(a.clone(), b.clone());
     // TODO: Fix modular inverse possibly being non-existent
@@ -264,6 +288,7 @@ mod quick_maths {
     type Output;
     fn modulo(&self, rhs: &RHS) -> Self::Output;
   }
+
   impl Modulo<BigInt> for BigInt {
     type Output = BigInt;
     fn modulo(&self, rhs: &BigInt) -> Self::Output {
@@ -280,6 +305,7 @@ mod quick_maths {
   #[cfg(test)]
   mod tests {
     use super::*;
+    use num::traits::FromPrimitive;
 
     #[test]
     fn div_works() {
